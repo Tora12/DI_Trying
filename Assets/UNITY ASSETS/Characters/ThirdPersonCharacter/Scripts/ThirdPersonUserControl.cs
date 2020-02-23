ab2eb7555ed90190a46f7fa47dc23c25e4a62c81
lliwwill1101 <60647531+lliwwill1101@users.noreply.github.com>
2020-02-23 02:29:53 -0800

    Locked Player in plane
    The way we had it locked before was unstable and required messing with the input axis, I made some small changes to the testing asset script to lock the character in place

--- Assets/UNITY ASSETS/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/UNITY ASSETS/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -47,7 +47,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         {
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            float v = 0.0f; // CrossPlatformInputManager.GetAxis("Vertical");
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
