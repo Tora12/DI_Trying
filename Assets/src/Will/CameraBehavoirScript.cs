75898fa85332975e25b5d420f7f5a373707e2666
lliwwill1101 <60647531+lliwwill1101@users.noreply.github.com>
2020-02-25 11:00:30 -0800

    Basic Camera Movement
    The camera now follows the player on the z axis

---
+++ Assets/src/Will/CameraBehavoirScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavoirScript : MonoBehaviour
+{
+    private Transform mainCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        mainCharacter=GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=new Vector3(18.29f,4.09f,mainCharacter.position.z);
+    }
+}
