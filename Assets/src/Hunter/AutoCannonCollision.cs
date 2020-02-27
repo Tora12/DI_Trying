24621d2451356c89660975172a0189b7a98cc581
Hunter Casteel <huntercasteel41961@gmail.com>
2020-02-27 11:13:10 -0800

    Added AutoCannonCollider
    Added a collider script to the auto cannon which destroys it when in contact with the player

---
+++ Assets/src/Hunter/AutoCannonCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCannonCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision) //gets called when the AutoCannon is collided with
+    {
+        if (collision.transform.gameObject.name == "ThirdPersonController") //checks if the AutoCannon collided with the player
+            Destroy(gameObject); //destroys the AutoCannon
+    }
+}
