2ec74c432a82e2ecf57c44ebba78f4ada8106dd9
lliwwill1101 <60647531+lliwwill1101@users.noreply.github.com>
2020-02-27 13:28:23 -0800

    Seralized camera offsets
    camera offsets are now editable in the scene

--- Assets/src/Will/CameraBehavoirScript.cs
+++ Assets/src/Will/CameraBehavoirScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraBehavoirScript : MonoBehaviour
 {
     private Transform mainCharacter;
+    [SerializeField] float xOffset=18.29f;
+    [SerializeField] float yOffset=3.59f;
+    [SerializeField] float zOffset=0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,6 @@ public class CameraBehavoirScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position=new Vector3(18.29f,mainCharacter.position.y+3.59f,mainCharacter.position.z);
+        transform.position=new Vector3(xOffset,mainCharacter.position.y+yOffset,mainCharacter.position.z+zOffset);
     }
 }
