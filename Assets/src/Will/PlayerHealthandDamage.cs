2ef1c81eceaa08fcf03e24901bb2803c5acf4b65
lliwwill1101 <60647531+lliwwill1101@users.noreply.github.com>
2020-03-10 11:17:17 -0700

    Player Drops Ragdoll When Damaged

---
+++ Assets/src/Will/PlayerHealthandDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthandDamage : MonoBehaviour
+{
+    public float health=100.0f;
+    public bool dead=false;
+    [SerializeField] private GameObject doll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health<=0 && !dead){
+            Instantiate(doll, transform);
+            dead=true;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "EnemyBullet")
+        {
+            Destroy(other.gameObject);
+            health = health - 10f;
+        }
+    }
+}
