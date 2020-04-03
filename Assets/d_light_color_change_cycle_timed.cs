e781bec954728657254728bad9d440174253a5a5
Zachary Benning <19519394+Zachary-Benning@users.noreply.github.com>
2020-04-02 23:09:11 -0700

    Lights_Rooms_Galore
    Added Directional Light that cycles.
    Added second Room ... basic
    Organized game objects by rooms

---
+++ Assets/d_light_color_change_cycle_timed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class d_light_color_change_cycle_timed : MonoBehaviour
+{
+    // Interpolate light color between two colors back and forth
+    float duration = 1.0f;
+    Color color0 = Color.red;
+    Color color1 = Color.blue;
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // set light color
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+    }
+}
