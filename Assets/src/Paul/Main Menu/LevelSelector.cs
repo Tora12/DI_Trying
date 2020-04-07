1b4501757bc5dd73c577f6705775f999989c992b
Paul Shannon <mewtwo532@gmail.com>
2020-04-07 13:25:07 -0600

    Enemy Spawn
    When you load the level, a random amount of enemies, spawn in random locations across the map. The range is still manual. The amount of enemies is also still user controlled.

---
+++ Assets/src/Paul/Main Menu/LevelSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public string Scene = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int random(int min, int max)
+    {
+        return Random.Range(min, max);
+    }
+
+    public void levelloader()
+    {
+        Application.LoadLevel(Scene);
+
+    }
+}
--- Assets/src/Paul/UI/EnemySpawn.cs
+++ Assets/src/Paul/UI/EnemySpawn.cs
@@ -6,13 +6,19 @@ public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private GameTriggers gameTriggers;
     [SerializeField] private GameObject[] enemyArray;
-    [SerializeField] private float min = 0.0f;
-    [SerializeField] private float max = 0.0f;
+    [SerializeField] private float minSpawnArea = 0.0f;
+    [SerializeField] private float maxSpawnArea = 0.0f;
+    public int minEnemyCount = 0;
+    public int maxEnemyCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        int max = Random.Range(minEnemyCount, maxEnemyCount);
+        for (int i = 0; i < max; i++)
+        {
+            Spawn();
+        }
     }
 
     // Update is called once per frame
@@ -26,7 +32,7 @@ public class EnemySpawn : MonoBehaviour
         int enemyCount = enemyArray.Length;
         int num = Random.Range(0, enemyCount);
         Vector3 spawn;
-        spawn = new Vector3(0.0f, 0.5f, Random.Range(min, max));
+        spawn = new Vector3(0.0f, 0.5f, Random.Range(minSpawnArea, maxSpawnArea));
         gameTriggers.spawnEntity(enemyArray[num], spawn, 0);
     }
 }
