7227cb305c81f4de535f0105e79ae612d092d44e
Paul Shannon <mewtwo532@gmail.com>
2020-04-08 11:27:52 -0600

    Enemy Health Bars, and Scale
    I added health bars to the enemies, and updated the scale of the enemies. Only the two towers have heath bars.

--- Assets/src/Paul/Tower's/TowerController.cs
+++ Assets/src/Paul/Tower's/TowerController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerController : MonoBehaviour
 {
-    public int Health = 100;
+    public float maxHealth = 10;
+    public float Health;
+    [SerializeField] private GameObject HealthBar;
+    [SerializeField] private Slider slider;
     private int EnemyDespawnTime = 0;
     private bool Dead = false;
 
     private int Damage = 10; //REMOVE WHEN JENNER GETS A DAMAGE VALUE FOR BULLETS
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        
+        Health = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = Health;
     }
 
     void Update()
@@ -22,6 +28,7 @@ public class TowerController : MonoBehaviour
         {
             //Prevents the Animation from constantly replaying.
             Dead = true;
+            Destroy(HealthBar);
             Destroy(gameObject, EnemyDespawnTime);
         }
     }
@@ -32,6 +39,7 @@ public class TowerController : MonoBehaviour
         {
             Destroy(other.gameObject);
             Health = Health - Damage;
+            slider.value = Health;
         }
     }
 }
