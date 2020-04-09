fd0724ba5cdb4afde1b4a32f2bcc30d6a02a104c
Paul Shannon <mewtwo532@gmail.com>
2020-04-09 11:48:04 -0600

    Added a test case

--- Assets/tst/Paul/TestScriptPaul.cs
+++ Assets/tst/Paul/TestScriptPaul.cs
@@ -3,38 +3,44 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+//needed to load the scene
 using UnityEngine.SceneManagement;
+//needed to load prefabs
+using UnityEditor;
 
 namespace Tests
 {
     public class TestScriptPaul
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void TestScriptPaulSimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
+		//put scene set up common to all tests here
+		[SetUp]
+		public void Setup()
+		{
+			//load a scene
+			SceneManager.LoadScene("Testing Scene 1", LoadSceneMode.Single);
+		}
+		//same for tear down
+		[TearDown]
+		public void Teardown()
+		{
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator TestScriptPaulWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            //GameObject gameObject = Resources.Load<GameObject>("Prefabs/Enemies/Enemies/Tower_GaussGun.prefab");
-           // yield return null;
-            //load a scene
-            SceneManager.LoadScene("Testing Scene", LoadSceneMode.Additive);
-            //find an object in scene
-            GameObject spawnEnemy = GameObject.Find("Canvas/Spawn Enemy");
-            
-            //spawnEnemy.EnemySpawn.Spawn();
-            //wait for a period of seconds
-            yield return new WaitForSeconds(5);
-            //return success
-            yield return null;
-        }
-    }
+		}
+
+		[UnityTest]
+		public IEnumerator EnemyFire()
+		{
+			//find an object in scene
+			GameObject enemy = GameObject.Find("Tower_RocketLauncher");
+			//get a component of a certain class from an object
+			TowerMovement movement = enemy.GetComponent<TowerMovement>();
+			//call a function from the class
+			movement.Fire();
+			//assert the truth of somthing (test fails if not true)
+			Assert.IsTrue(GameObject.FindGameObjectWithTag("EnemyBullet"));
+			//wait for a period of seconds
+			yield return new WaitForSeconds(5);
+			//return success
+			yield return null;
+		}
+	}
 }
