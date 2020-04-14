26a4cf73ad88be2090566f11ff86a81e40a23726
Paul Shannon <mewtwo532@gmail.com>
2020-04-13 19:53:33 -0600

    Pause Menu

---
+++ Assets/src/Paul/UI/Conformation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Conformation : MonoBehaviour
+{
+    public void yesExit()
+    {
+        Application.Quit();
+    }
+
+    public void yesMainMenu()
+    {
+        SceneManager.LoadScene("Menu Scene");
+    }
+}
---
+++ Assets/src/Paul/UI/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject player;
+    private GameObject clone;
+    private bool paused = false;
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Escape") && !paused)
+        {
+            paused = true;
+            player.SetActive(false);
+            clone = Instantiate(pauseMenu);
+        }
+        else if (Input.GetButtonDown("Escape") && paused)
+        {
+            resume();
+        }
+    }
+
+    public void resume()
+    {
+        paused = false;
+        Destroy(clone);
+        player.SetActive(true);
+    }
+}
