632610ae1c39172a1fa464e4fafb1f0cd008e81c
Paul Shannon <mewtwo532@gmail.com>
2020-04-15 20:17:55 -0600

    Pause Menu Fix

---
+++ Assets/src/Paul/PauseMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private Pause pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause = GameObject.Find("EventSystem").gameObject.GetComponent<Pause>();
+    }
+
+
+    public void click()
+    {
+        pause.resume();
+    }
+}
