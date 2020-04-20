9065f347a3a91ae803ac92b1c354f9eafdb7fe26
Paul Shannon <mewtwo532@gmail.com>
2020-04-20 09:44:47 -0600

    Spikes Damage

---
+++ Assets/src/Paul/Spikes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Spikes : MonoBehaviour
+{
+    public float damage = 10.0f;
+    private PlayerHealthandDamage playerHealth;
+    private bool playerContact = false;
+    private Collision player;
+
+    private void FixedUpdate()
+    {
+        if (playerContact)
+        {            
+            playerHealth.health -= damage;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.gameObject.CompareTag("Player"))
+        {
+            player = collision;
+            playerContact = true;
+            playerHealth = player.transform.gameObject.GetComponent<PlayerHealthandDamage>();
+        }
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.gameObject.CompareTag("Player"))
+        {
+            playerContact = false;
+        }
+    }
+}
