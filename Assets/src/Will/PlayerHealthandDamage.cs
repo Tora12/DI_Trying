ccefd2c843e8895e1eef9c5403d2607621898d29
lliwwill1101 <60647531+lliwwill1101@users.noreply.github.com>
2020-04-21 10:13:25 -0700

    Player damage and health bar.
    Player gets damage number from projectile and has a health bar

--- Assets/src/Will/PlayerHealthandDamage.cs
+++ Assets/src/Will/PlayerHealthandDamage.cs
@@ -1,36 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthandDamage : MonoBehaviour
 {
     public float health=100.0f;
     public bool dead=false;
     [SerializeField] private GameObject doll=null;
+	[SerializeField] private GameObject HealthBar;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private Slider slider;
 	
-	float old_health;
+	float max_health;
     // Start is called before the first frame update
     void Start()
     {
-        old_health=health;
+        max_health=health;
+		slider.maxValue = max_health;
+        slider.value = health;
+		
     }
 
     // Update is called once per frame
     void Update()
     {
+		
 		if(transform.position.y<-20) health=0;
         if(health<=0 && !dead){
             Instantiate(doll, transform.position, transform.rotation);
 			gameObject.SetActive(false);
             dead=true;
         }
+		if (health < max_health)
+        {
+            canvas.SetActive(true);
+            HealthBar.SetActive(true);
+        }
+		slider.value=health;
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "EnemyBullet")
         {
+			health -=other.GetComponent<EnemyBullet>().damage;
             Destroy(other.gameObject);
-            health -=10f;
+            
         }
     }
 	public void killPlayer(){
@@ -38,7 +53,9 @@ public class PlayerHealthandDamage : MonoBehaviour
 	}
 	public void respawnPlayer(){
 		dead=false;
-		health=old_health;
+		health=max_health;
 		gameObject.SetActive(true);
+		canvas.SetActive(false);
+        HealthBar.SetActive(false);
 	}
 }
