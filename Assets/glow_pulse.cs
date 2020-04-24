896ffa3a7ee68a144f4608b07bdde43d8765fa6a
Zachary Benning <19519394+Zachary-Benning@users.noreply.github.com>
2020-04-24 14:03:53 -0700

    glow features
    added a glow feature.

---
+++ Assets/glow_pulse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class glow_pulse : MonoBehaviour
+{
+    public PostProcessVolume ppProfile;
+
+    private void Start()
+    {
+        changeProfileWeight();
+    }
+
+    void changeProfileWeight()
+    {
+        ppProfile.weight = 0.0f;
+    }
+
+}
