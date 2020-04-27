9db76d1dda221aeedce4256676b30cf6c60936cc
Zachary Benning <19519394+Zachary-Benning@users.noreply.github.com>
2020-04-26 18:34:41 -0700

    crystal_glow

---
+++ Assets/src/Zac/crystal_glow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crystal_glow : MonoBehaviour
+{
+    float duration = 3.0f;
+    float originalRange;
+
+    Light lt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        originalRange = lt.range;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var amplitude = Mathf.PingPong(Time.time, duration);
+        amplitude = amplitude / duration * 0.5f + 0.5f;
+
+        lt.range = originalRange * amplitude;
+    }
+}
