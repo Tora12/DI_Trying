1023123b1582a2e74fbbd04b216840435d238ed4
Zachary Benning <19519394+Zachary-Benning@users.noreply.github.com>
2020-04-28 15:53:56 -0700

    add_sounds

---
+++ Assets/play_sound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play_sound : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
