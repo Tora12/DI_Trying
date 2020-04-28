8c09c513c00a805f8ccde957d80b05c2b377d745
Zachary Benning <19519394+Zachary-Benning@users.noreply.github.com>
2020-04-28 15:01:35 -0700

    added_sound

---
+++ Assets/audio_que.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_que : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "audio_trigger")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
