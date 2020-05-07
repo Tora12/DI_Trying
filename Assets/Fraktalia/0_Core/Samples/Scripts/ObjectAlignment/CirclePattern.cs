bc347ad629187e6bb8bd4cd3c6b26901e111314f
Zachary Benning <19519394+Zachary-Benning@users.noreply.github.com>
2020-05-06 19:32:29 -0700

    Got_upgrade_asset

---
+++ Assets/Fraktalia/0_Core/Samples/Scripts/ObjectAlignment/CirclePattern.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fraktalia.Core.Samples
+{
+    public class CirclePattern : PatternGenerator
+    {
+
+        public float Expansion;
+        public float startangle = 0;
+
+        public override Vector3 Pattern(float Index, float childcount)
+        {
+
+
+
+            float Angle = (360.0f / childcount) * Index;
+            Angle += startangle;
+            Vector3 output = new Vector2();
+            output.y = 0;
+            output.z = Mathf.Sin(Angle * Mathf.PI / 180);
+            output.x = Mathf.Cos(Angle * Mathf.PI / 180);
+            output *= Expansion / 100;
+
+            return output;
+        }
+    }
+}
---
+++ Assets/Fraktalia/0_Core/Samples/Scripts/SceneMovement/NoClipFirstPersonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fraktalia.Core.Samples
+{
+    public class NoClipFirstPersonController : MonoBehaviour
+    {
+
+        public float movementForwardMultiplier = 4f;
+        public float movementSideMultiplier = 4f;
+
+        private string forwardAxis = "Vertical";
+        private string horizontalAxis = "Horizontal";
+
+        void FixedUpdate()
+        {
+            float forwardMovement = Input.GetAxis(forwardAxis) * movementForwardMultiplier;
+            float horizontalMovement = Input.GetAxis(horizontalAxis) * movementSideMultiplier;
+            Vector3 movementDelta = new Vector3(horizontalMovement, 0, forwardMovement);
+            GetComponent<Rigidbody>().AddRelativeForce(movementDelta);
+        }
+    }
+}
